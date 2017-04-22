664da46f4233ad7f417819baa3c5f9ade118badc
ipc103 <iancandysemail@gmail.com>
2017-04-22 14:01:36 -0400

    change to top tracks

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router'
 
 import App from './components/App'
-import User from './components/User'
+import TopTracks from './components/TopTracks'
 
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App} />
-    <Route path="/users/:accessToken/:refreshToken" component={User} />
+    <Route path="/users/:accessToken/:refreshToken" component={TopTracks} />
   </Router>,
   document.getElementById('root')
 );
