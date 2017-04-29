c037f79a635593ddb8f81eaac2d5433e8278a7ec
ipc103 <iancandysemail@gmail.com>
2017-04-29 12:44:13 -0400

    add actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,16 @@
-import { getTopTracks } from '../requests'
-import { FETCH_TRACKS } from './types'
+import { getTopTracks, getTopArtists } from '../requests'
+import { FETCH_TRACKS, FETCH_TOP_ARTISTS } from './types'
+
 export function fetchTopTracks(){
   return dispatch => {
     getTopTracks()
       .then( res => dispatch({type: FETCH_TRACKS, payload: res.data.items }))
+  }
+}
+
+export function fetchTopArtists(){
+  return dispatch => {
+    getTopArtists()
+      .then( res => dispatch({type: FETCH_TOP_ARTISTS, payload: res.data.items }))
   }
 }
